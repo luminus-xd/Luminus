b2955fcfcd77846e01a6c1cd673407211c9e6b94
eleline <eleline.dev@gmail.com>
2023-01-09 15:25:40 +0900

    scss 設定

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import css from "styles/Top.module.scss";
+
+const Top: NextPage = () => {
+  return (
+    <div>
+      <div className={css.container}>sss</div>
+      <h1>My Template</h1>
+    </div>
+  );
+};
+
+export default Top;
