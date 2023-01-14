dbcade673eee092f7f65480a9b830517c3e873ef
eleline <eleline.dev@gmail.com>
2023-01-15 04:35:57 +0900

    public アセット画像のディレクトリ調整

--- src/components/_hero.tsx
+++ src/components/_hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="relative block bg-lightest">
         <img
           className="w-full pointer-events-none select-none"
-          src="/pict-twitter-header.jpg"
+          src="/images/pict-twitter-header.jpg"
           alt="Twitterヘッダー"
           decoding="async"
           width="1500"
@@ -13,7 +13,7 @@ const Hero = () => {
         <div className="absolute -bottom-12 md:-bottom-24 left-[50%] -translate-x-2/4 w-24 md:w-48 h-24 md:h-48">
           <img
             className="rounded-full border-4 border-lightest pointer-events-none select-none"
-            src="/pict-twitter-icon.jpg"
+            src="/images/pict-twitter-icon.jpg"
             alt="Twitterアイコン"
             decoding="async"
             width="200"
