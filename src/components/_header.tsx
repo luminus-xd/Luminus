dcb0a2331d66af44ba0bcaff60e2227f1db91f20
eleline <eleline.dev@gmail.com>
2023-01-15 04:10:27 +0900

    コンポーネント分割

---
+++ src/components/_header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="absolute z-10">
+      <h1 className="text-xl text-lightest md:text-4xl font-bold p-2 md:p-3">
+        Luminus.
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
